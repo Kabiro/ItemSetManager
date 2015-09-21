bde8c61ad30dbfbc9da50b9619a848514be4a630
kabiro <kabiro.bis@gmail.com>
2015-09-22 01:08:06 +0200

    Retrieve properly heroku database url and use hikari datasource

--- src/main/java/fr/kabiro/lol/ism/config/Config.java
+++ src/main/java/fr/kabiro/lol/ism/config/Config.java
@@ -1,11 +1,18 @@
 package fr.kabiro.lol.ism.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaxxer.hikari.HikariDataSource;
 import fr.kabiro.lol.ism.core.model.Region;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class Config {
@@ -25,4 +32,30 @@ public class Config {
         return Region.EUW;
     }
 
+    @Bean
+    public DataSource dataSource(Environment environment) throws URISyntaxException, ClassNotFoundException {
+        String herokuUrl = environment.getProperty("DATABASE_URL");
+        String springDatasourceUrl = environment.getProperty("spring.datasource.url");
+        HikariDataSource ds = new HikariDataSource();
+
+        if (!StringUtils.isEmpty(herokuUrl)){                   //Heroku postgres database
+            URI uri = new URI(herokuUrl);
+            String username = uri.getUserInfo().split(":")[0];
+            String password = uri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + uri.getHost() + ':' + uri.getPort() + uri.getPath();
+
+            ds.setJdbcUrl(dbUrl);
+            ds.setUsername(username);
+            ds.setPassword(password);
+        }
+        else if (!StringUtils.isEmpty(springDatasourceUrl)){    //Use standard config
+            ds.setJdbcUrl(springDatasourceUrl);
+            ds.setUsername(environment.getProperty("spring.datasource.username"));
+            ds.setPassword(environment.getProperty("spring.datasource.password"));
+        }
+        else{                                                   //Fallback to h2
+            ds.setJdbcUrl("jdbc:h2:mem:");
+        }
+        return ds;
+    }
 }
