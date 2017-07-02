d4bb8c5da7c62d386eee75aa8a1f111efd5e2ba5
kabiro <kabiro.bis@gmail.com>
2017-07-02 15:33:15 +0200

    optimize vendor

--- src/main/ui/app/scripts/vendor.ts
+++ src/main/ui/app/scripts/vendor.ts
@@ -4,3 +4,7 @@ import "@uirouter/angularjs";
 import "angular-ui-bootstrap";
 
 import "../styles/vendor.scss"
+
+import "@angular/upgrade/static";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
